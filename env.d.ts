ee5a6189647b79e7706bee2cb000576c330e6da9
DESKTOP-64649GT\Master <crm03008@gmail.com>
2024-05-23 02:25:21 +0900

    feat: mailjs, sweet alert 연동
    - mailjs 연동
    - sweet alert2 라이브러리 연동
    - form 태그를 ref로 연동, useState 삭제

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_MAIL_SERVICE_KEY: string;
+    NEXT_PUBLIC_MAIL_TEMPLATE_KEY: string;
+    NEXT_PUBLIC_MAIL_PRIVATE_KEY: string;
+  }
+}
