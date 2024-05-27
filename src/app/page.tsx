ce8441fe97a0e78e300066462c82eaf12bb920a0
지인성 <crm03008@gmail.com>
2024-05-27 17:35:29 +0900

    feat: 온보딩 히어로 섹션 추가
    - 온보딩 히어로 섹션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 import { AboutSection } from "@/pages-flat/about";
 import { ProjectSection } from "@/pages-flat/project";
 import { StackPage } from "@/pages-flat/stack";
+import { HeroSection } from "@/pages-flat/onBoarding/Hero";
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <AboutSection />
       <ProjectSection preview={true} />
       <StackPage />
