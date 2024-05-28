ce4690b87af125cefdd9ad98d5cc16cd10a30c47
지인성 <crm03008@gmail.com>
2024-05-28 17:51:03 +0900

    refactor: about 페이지 개선, Hero 페이지 개선, project 정보 페이지 변경
    - about 페이지 개선, Hero 페이지 개선, project 정보 페이지 변경

--- src/pages-flat/about/AboutSection.tsx
+++ src/pages-flat/about/AboutSection.tsx
@@ -9,21 +9,24 @@ export default function AboutSection() {
       <header className={styles.header}>
         <h1>About Me</h1>
       </header>
-      <div className={styles.hero}>
+      <main className={styles.hero}>
         <div className={styles.about}>
           <div className={styles.content}>
             <h1>
               안녕하세요, 프론트엔드 개발자 <br />
               지인성 입니다.
             </h1>
-            <h2 className={styles.middleTitle}>저는 이런 사람 입니다.</h2>
             <p>
-              ✅ 성장을 위한 새로운 기술과 도전을 두려워하지 않습니다.
+              ✅ 성장을 위한 새로운 기술 학습과 도전을 두려워하지 않습니다.
               <br />
-              <br />✅ 동료들과 서비스를 만들어가고 문제를 해결하는 과정을
-              즐깁니다.
+              <br />✅ 사용자 중심의 접근 방식을 중요하게 생각하며, 최고의
+              사용자 경험을 제공하기 위해 노력합니다.
               <br />
-              <br />✅
+              <br />✅ 팀워크와 협업을 통해 더 나은 결과를 만들어내는 것을
+              목표로 합니다.
+              <br />
+              <br />✅ 열린 소통과 피드백을 통해 함께 발전하는 문화를
+              지향합니다.
             </p>
           </div>
         </div>
@@ -33,7 +36,33 @@ export default function AboutSection() {
             <Image src={profile} alt="profile" />
           </div>
         </div>
-      </div>
+      </main>
+      <section className={styles.eachSection}>
+        <header className={styles.header}>
+          <h1>Certificate / Awards</h1>
+        </header>
+        <main className={styles.certificate}>
+          <ul>
+            <li>📜 2023.12.15 SQLD (한국데이터산업진흥원)</li>
+            <li>
+              🏆 2023.11.24 삼성 청년 SW 아카데미 10기 1학기 프로젝트 최우수상
+            </li>
+            <li>📜 2022.11.25 정보처리기사 (한국산업인력공단)</li>
+          </ul>
+        </main>
+      </section>
+      <section className={styles.eachSection}>
+        <header className={styles.header}>
+          <h1>Education / Experience</h1>
+        </header>
+
+        <main className={styles.education}>
+          <ul>
+            <li>💻 2023.07 ~ 삼성 청년 SW 아카데미 10기</li>
+            <li>💻 2015.03 ~ 2022.02 단국대학교 소프트웨어 공학과 </li>
+          </ul>
+        </main>
+      </section>
     </section>
   );
 }
