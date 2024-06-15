e15150edfc5411a2ce4410383254c050516161fd
DESKTOP-64649GT\Master <crm03008@gmail.com>
2024-06-16 00:03:19 +0900

    fix: 경로 버그 수정
    - 경로 명 수정

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
           <LinkButton href="/project" title="Project" />
         </li>
         <li>
-          <LinkButton href="/stack" title="Skills" />
+          <LinkButton href="/skills" title="Skills" />
         </li>
         <li>
           <LinkButton href="/contact" title="Contact" />
