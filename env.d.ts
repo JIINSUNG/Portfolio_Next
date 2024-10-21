568b4af55097901927bb2021e76c7356f2b66ddd
JIINSUNG <crm03008@gmail.com>
2024-10-21 15:54:13 +0900

    feat : 채널톡 추가

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,6 @@ namespace NodeJS {
     NEXT_PUBLIC_MAIL_SERVICE_KEY: string;
     NEXT_PUBLIC_MAIL_TEMPLATE_KEY: string;
     NEXT_PUBLIC_MAIL_PRIVATE_KEY: string;
+    NEXT_PUBLIC_CHANNEL_TALK_KEY: string;
   }
 }
